73c60ea4f00c397e30d8c9eb511116e070e939ee
taliberman5 <taliberman5@gmail.com>
2024-12-03 22:55:09 +0200

    updated to support the limitation

--- step2.py
+++ step2.py
@@ -1,41 +1,135 @@
-import pandas as pd
+import heapq
 import time
-from utilis import merge_sort, write_execution_time
+import csv
+
+from utilis import calc_num_chunks, write_execution_time
+import pandas as pd
 
 
-
-def sort_by_chunks(file_name, chunk_size, key):
+def merge_files(output_file, k, key_pos, header):
     """
-    Sort the file name allowing processing only chunk_size records at a time
+    Merge K files into one file
 
-    :param file_name: The name of the CSV file
-    :param chunk_size: Number of rows to process in parallel
+    :param output_file: The sorted output file
+    :param k: The number of files to merge
+    :param key_pos: The key positin on which the sort should be done
+    :param header: File header
+    """
+    harr = []
+    out = open(output_file, "w")
+    out.write(header)
+
+    # Open output files in read mode.
+    in_files = [open(str(i), 'r') for i in range(k)]
+    in_files2 = [open(str(i), 'r') for i in range(k)]
+    in_files_csv = [csv.reader(in_files2[i]) for i in range(k)]
+
+    # Create a min heap with k heap nodes.
+    # Every heap node has first element of scratch output file
+    for i in range(k):
+        header = next(in_files[i])   # pass the header
+        header = next(in_files_csv[i])   # pass the header
+        element = in_files[i].readline().strip()
+        if element:
+            key_val = next(in_files_csv[i])[key_pos]
+            heapq.heappush(harr, (key_val, element, i))
+
+    count = 0
+    while count < k:
+        # Get the minimum element and store it in output file
+        root = heapq.heappop(harr)
+        out.write(root[1] + '\n')
+
+        # Find the next element that will
+        # replace current root of heap.
+        element = in_files[root[2]].readline().strip()
+        if element:
+            key_val = next(in_files_csv[root[2]])[key_pos]
+            heapq.heappush(harr, (key_val, element, root[2]))
+        else:
+            count += 1
+
+    # close input and output files
+    for i in range(k):
+        in_files[i].close()
+    out.close()
+
+
+def create_initial_runs(input_file, key, chunk_size, num_chunks):
+    """
+    Using a merge-sort algorithm, create the
+    initial runs and divide them evenly among the output files
+
+    :param input_file:
     :param key: The key on which the sort should be done
-    :return: Sorted dataframe
+    :param chunk_size: Number of chunks needed to process all data
+    :param num_chunks: Number of rows to process in parallel
     """
-    #Initiate an empty dataframe
-    df = pd.DataFrame()
+    in_file = open(input_file, "r")
 
-    # Load a CSV file in chunks of chunk_size records
-    for chunk in pd.read_csv(file_name, chunksize=chunk_size):
+    # output scratch files
+    out_files = [open(str(i), 'w') for i in range(num_chunks)]
+
+    next_output_file = 0
+
+    for chunk in pd.read_csv(input_file, chunksize=chunk_size):
+        # Sort each chunk
         chunk = chunk.sort_values(key)
-        df = merge_sort(df, chunk, key)
 
-    return df
+        # write the records to the appropriate scratch output file
+        chunk.to_csv(str(next_output_file), index=False)
+
+        next_output_file += 1
+
+    # close input and output files
+    for i in range(num_chunks):
+        out_files[i].close()
+
+    in_file.close()
 
 
+def external_sort(input_file, output_file, key, num_chunks, chunk_size):
+    """
+    For sorting data stored on disk
 
-if __name__ == "__main__":
+    :param input_file:
+    :param output_file:
+    :param key: The key on which the sort should be done
+    :param num_chunks: Number of chunks needed to process all data
+    :param chunk_size: Number of rows to process in parallel
+    """
+    # Read the input file, create the initial runs,
+    # and assign the runs to the scratch output files
+    create_initial_runs(input_file, key, chunk_size, num_chunks)
+
+    # Get the key position from the header
+    in_file = open(input_file, "r")
+    header = next(in_file)
+    headers = header.strip().split(",")
+    key_pos = headers.index(key)
+
+    # Merge the runs using the K-way merging
+    merge_files(output_file, num_chunks, key_pos, header)
+
+
+def main():
     # Save the start time of the execution
     start = time.time()
 
-    df = sort_by_chunks('sample.csv', chunk_size=2000, key='data')
+    input_file = 'sample.csv'
+    output_file = 'sorting-step2.csv'
+    key = 'data'
 
-    # Save the result to a new csv file
-    df.to_csv('sorting-step2.csv', index=False)
+    # The size of each partition
+    chunk_size = 2000
+    num_chunks = calc_num_chunks(input_file, chunk_size)
+
+    external_sort(input_file, output_file, key, num_chunks, chunk_size)
 
     # Save the end time of the execution
     end = time.time()
     write_execution_time('sorting-step2_process_time.txt', start, end)
 
 
+if __name__ == "__main__":
+    main()
